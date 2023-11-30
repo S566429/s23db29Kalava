e6236a562e3b8747d0939f34a3d392ce7251fe43
S566429 <143213608+S566429@users.noreply.github.com>
2023-11-29 21:23:35 -0600

    Addded changes

--- models/icecream.js
+++ models/icecream.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose")
-const icecreamSchema = mongoose.Schema({
-    icecream_flavour: String,
-    icecream_quantity: String,
-    icecream_cost: Number
+var icecreamSchema = mongoose.Schema({
+    icecream_flavour: {type:String , required: true},
+    icecream_quantity: { type:String , required: true, maxLength:[3]},
+    icecream_cost: {type: Number, required: true, min:50, max:5000}
 })
-module.exports = mongoose.model("icecream",icecreamSchema)
+module.exports = mongoose.model("icecream",
+icecreamSchema)
+
+
+
 
--- routes/icecream.js
+++ routes/icecream.js
@@ -1,14 +1,24 @@
 var express = require('express');
 const icecream_controlers= require('../controllers/icecream');
 var router = express.Router();
+const passport = require('passport');
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 /* GET costumes */
 router.get('/', icecream_controlers.icecream_view_all_Page );
 /* GET detail costume page */
-router.get('/detail', icecream_controlers.icecream_view_one_Page);
+router.get('/detail',secured, icecream_controlers.icecream_view_one_Page);
 /* GET create costume page */
-router.get('/create', icecream_controlers.icecream_create_Page);
+router.get('/create',secured, icecream_controlers.icecream_create_Page);
 /* GET create update page */
-router.get('/update', icecream_controlers.icecream_update_Page);
+router.get('/update',secured, icecream_controlers.icecream_update_Page);
 /* GET delete costume page */
-router.get('/delete', icecream_controlers.icecream_delete_Page);
+router.get('/delete',secured, icecream_controlers.icecream_delete_Page);
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+    });
 module.exports = router;
